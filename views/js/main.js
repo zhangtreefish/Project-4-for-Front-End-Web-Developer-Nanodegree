b26b61f35ee8535084d47b8bef94220e8c715c2a
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-06-07 14:41:29 -0500

    fix(main.js):move declarations outside for loop of EventListener

--- views/js/main.js
+++ views/js/main.js
@@ -544,12 +544,12 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
+  var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
+  for (var i = 0; i < 200; i++) {
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
