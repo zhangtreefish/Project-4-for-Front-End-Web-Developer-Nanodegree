ddac34544ac8b43b7166b4ab6ad2b40edb5f8063
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-08-18 22:14:13 -0500

    optimize(mainV1-8-18)

--- views/js/mainV1-8-18.js
+++ views/js/mainV1-8-18.js
@@ -502,7 +502,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var movingPizzas=document.getElementById("movingPizzas1");
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 50; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
