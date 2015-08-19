7d38f25ca3ae09e9c468eb99fb422f48a63570e7
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-08-18 22:01:58 -0500

    reduce pizza number in the event listerner method;removed style.height

--- views/js/mainV1-8-18.js
+++ views/js/mainV1-8-18.js
@@ -478,6 +478,7 @@ function updatePositions() {
   var phaseBase=document.body.scrollTop / 1250;
   for (var i = 0; i < num; i++) {
     var phase = Math.sin(phaseBase + (i % 5));
+
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -495,18 +496,21 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
+//Here I moved movingPizzas DOM construction outside the for loop
+//and added style information to .mover css stle
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var movingPizzas=document.getElementById("movingPizzas1");
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
-    elem.style.height = "100px";
+    //elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
